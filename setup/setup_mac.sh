87da00e9802c4a62a04ca6b19c1ab6443cb59f26
uchikun2493 <um2493@gmail.com>
2019-06-06 20:07:20 +0900

    deleted setup_mac.sh and add. setup_oni.sh

--- setup/setup_mac.sh
+++
@@ -1,4 +0,0 @@
-# あらかじめXcodeをアプデしておくこと
-brew install vim
-echo export PATH="/usr/local/Cellar:$PATH" >> ~/.bashrc
-source ~/.bashrc
---
+++ setup/setup_oni.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim

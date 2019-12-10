9e791a15f8d82b3ef8f14d469de72a11904d6bba
uchikun2493 <um2493@gmail.com>
2019-12-10 21:43:18 +0900

    add file: config.fish

---
+++ config.fish
@@ -0,0 +1,30 @@
+
+## alias
+# bdで親ディレクトリに戻る
+alias bd='cd ..'
+# exit
+alias q='exit'
+alias :q='exit'
+alias :Q='exit'
+# clear
+alias c='clear'
+alias C='clear'
+
+## git command 
+alias ga='git add'
+alias gc='git commit -m'
+
+## cdしたらls
+functions --copy cd standard_cd
+function cd
+    standard_cd $argv; and ls -a
+end
+
+## path
+# pyenv
+set PATH $HOME/.pyenv/bin $PATH
+eval (pyenv init - | source)
+
+# 自分で入れたvimのパスを通す
+set PATH /usr/local/Cellar/vim/8.1.1700/bin $PATH
+
